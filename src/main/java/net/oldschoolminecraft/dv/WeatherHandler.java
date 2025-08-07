9702c94a3f3ffed646bb02ad369d4c98834b3bac
SavageUser <LimitlessPowerHD@gmail.com>
2025-08-07 19:38:28 -0400

    Update WeatherHandler.java
    Added debug message.

--- src/main/java/net/oldschoolminecraft/dv/WeatherHandler.java
+++ src/main/java/net/oldschoolminecraft/dv/WeatherHandler.java
@@ -3,8 +3,6 @@ package net.oldschoolminecraft.dv;
 import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.event.weather.WeatherListener;
 
-import java.io.File;
-
 public class WeatherHandler extends WeatherListener
 {
     public void onWeatherChange(WeatherChangeEvent event)
@@ -12,6 +10,7 @@ public class WeatherHandler extends WeatherListener
         if (event.toWeatherState() && !DayVote.getInstance().shouldWeatherBeOn)
         {
             System.out.println("[DayVote] Weather change event was cancelled as it was not voted on.");
+            DayVote.getInstance().sendDebugMessage("Weather change event was cancelled as it was not voted on.");
             event.setCancelled(true);
             return;
         }
